9115a8f16d05db8dab269333ee5068d22d0c347c
Demons Light <jetaimefrc@gmail.com>
2020-01-06 22:47:59 +0700

    chore: add graphql appolo server

---
+++ api-gateway/src/graphql/resolvers/Query/listings.js
@@ -0,0 +1,16 @@
+const listingsResolver = async () => {
+  return [
+    {
+      id: 1,
+      title: 'AAA',
+      description: 'BBB',
+    },
+    {
+      id: 2,
+      title: 'CCC',
+      description: 'DDD',
+    },
+  ]
+}
+
+export default listingsResolver
