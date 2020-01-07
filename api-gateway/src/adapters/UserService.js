a4b8f24b78cbd60429b4a17fea6e311a5e196c97
Demons Light <jetaimefrc@gmail.com>
2020-01-07 20:36:12 +0700

    chore: create user services

---
+++ api-gateway/src/adapters/UserService.js
@@ -0,0 +1,15 @@
+import got from 'got'
+
+const USERS_SERVICE_API = 'http://users-service:7101'
+
+export default class UsersService {
+  static async createUser({ email, password }) {
+    const body = await got.post(`${USERS_SERVICE_API}/users`, { json: { email, password } }).json()
+    return body
+  }
+
+  static async fetchUser({ userId }) {
+    const body = await got.get(`${USERS_SERVICE_API}/users/${userId}`).json()
+    return body
+  }
+}
