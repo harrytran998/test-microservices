e0a7b33c0b06a895705a6d6d2c9c28b3ece04fec
Demons Light <jetaimefrc@gmail.com>
2020-01-07 20:44:31 +0700

    chore: add models User vs User Session

---
+++ users-service/src/db/models.js
@@ -0,0 +1,59 @@
+import { DataTypes, Model } from 'sequelize'
+
+import sequelize from './connection'
+
+export class User extends Model {}
+User.init(
+  {
+    id: {
+      allowNull: false,
+      primaryKey: true,
+      type: DataTypes.UUID,
+    },
+    email: {
+      allowNull: false,
+      type: DataTypes.STRING,
+      unique: true,
+    },
+    password: {
+      allowNull: false,
+      type: DataTypes.STRING,
+    },
+  },
+  {
+    defaultScope: {
+      rawAttributes: { exclude: ['password'] },
+    },
+    modelName: 'users',
+    sequelize,
+  },
+)
+
+export class UserSession extends Model {}
+UserSession.init(
+  {
+    id: {
+      allowNull: false,
+      primaryKey: true,
+      type: DataTypes.UUID,
+    },
+    userId: {
+      allowNull: false,
+      references: {
+        key: 'id',
+        model: 'users',
+      },
+      type: DataTypes.UUID,
+    },
+    expiresAt: {
+      allowNull: false,
+      type: DataTypes.DATE,
+    },
+  },
+  {
+    modelName: 'userSessions',
+    paranoid: false,
+    sequelize,
+    updatedAt: false,
+  },
+)
