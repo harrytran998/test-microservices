b99793351a1cf41b7e0d2131f655f470385d78f3
Demons Light <jetaimefrc@gmail.com>
2020-01-07 20:00:19 +0700

    chore: init basic client

---
+++ client-app/src/components/Root/Root.js
@@ -0,0 +1,7 @@
+import React, { useEffect, useState } from 'react'
+
+const Root = () => {
+  return <h1>XXX</h1>
+}
+
+export default Root
--- client-app/src/index.js
+++ client-app/src/index.js
@@ -1,4 +1,5 @@
-import react from 'react'
+import React from 'react'
 import { render } from 'react-dom'
+import Root from '#root/components/Root'
 
-render('Working', document.getElementById('app'))
+render(<Root />, document.getElementById('app'))
