2c4ae1e8cccb4843b71166055c660a171791c519
Demons Light <jetaimefrc@gmail.com>
2020-01-07 22:52:17 +0700

    chore: update api gateway

--- api-gateway/src/server/startServer.js
+++ api-gateway/src/server/startServer.js
@@ -12,6 +12,7 @@ import formatGraphQLErrors from './formatGraphQLErrors'
 const PORT = accessEnv('PORT', 7000)
 
 const apolloServer = new ApolloServer({
+  context: fuck => fuck,
   formatError: formatGraphQLErrors,
   resolvers,
   typeDefs,
