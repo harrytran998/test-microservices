4c11d6fa02acab8c0d91e08183019e8481a9641d
Demons Light <jetaimefrc@gmail.com>
2020-01-07 22:09:32 +0700

    chore(deps): add styled components

--- client-app/src/components/Root/Root.js
+++ client-app/src/components/Root/Root.js
@@ -1,7 +1,39 @@
 import React, { useEffect, useState } from 'react'
+import styled from 'styled-components'
+
+const Container = styled.div`
+  display: flex;
+  flex-flow: row nowrap;
+  margin: 0 auto;
+  width: 80rem;
+`
+
+const Content = styled.div`
+  flex: 1;
+  margin-right: 1rem;
+`
+
+const Sidebar = styled.div`
+  flex: 0 auto;
+  width: 10rem;
+`
+
+const Wrapper = styled.div`
+  box-sizing: border-box;
+  height: 100%;
+  padding: 1rem;
+  width: 100%;
+`
 
 const Root = () => {
-  return <h1>XXX</h1>
+  return (
+    <Wrapper>
+      <Container>
+        <Content>AAA</Content>
+        <Sidebar>AAAA</Sidebar>
+      </Container>
+    </Wrapper>
+  )
 }
 
 export default Root
