86ceafea4bcc682cfd594af773bbd05682464475
Demons Light <jetaimefrc@gmail.com>
2020-01-07 20:36:45 +0700

    chore: add get - native method lodash

---
+++ api-gateway/src/helpers/getLodash.js
@@ -0,0 +1,9 @@
+const get = (obj, path, defaultValue) => {
+  const result = String.prototype.split
+    .call(path, /[,[\].]+?/)
+    .filter(Boolean)
+    .reduce((res, key) => (res !== null && res !== undefined ? res[key] : res), obj)
+  return result === undefined || result === obj ? defaultValue : result
+}
+
+export default get
