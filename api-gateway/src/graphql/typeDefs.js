89d8b1cf0c79bb8f50708a99e0058adf0588319d
Demons Light <jetaimefrc@gmail.com>
2020-01-07 22:52:04 +0700

    chore: update typedefs graphql

--- api-gateway/src/graphql/typeDefs.js
+++ api-gateway/src/graphql/typeDefs.js
@@ -1,6 +1,8 @@
 import { gql } from 'apollo-server'
 
 const typeDefs = gql`
+  scalar Date
+
   type Listing {
     id: ID!
     description: String!
@@ -10,12 +12,22 @@ const typeDefs = gql`
     id: ID!
     email: String!
   }
+
+  type UserSession {
+    id: ID!
+    user: User!
+    createdAt: Date!
+    expiresAt: Date!
+  }
+
   type Query {
     listings: [Listing!]!
     users: [User!]!
   }
+
   type Mutation {
     createUser(email: String!, password: String!): User!
+    createUserSession(email: String!, password: String!): UserSession!
   }
 `
 
