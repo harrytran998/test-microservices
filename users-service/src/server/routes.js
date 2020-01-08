47da06429df1c6ee61d7430d412c9e6368ec2b38
Demons Light <jetaimefrc@gmail.com>
2020-01-08 20:46:06 +0700

    chore: add get detail session with ID

--- users-service/src/server/routes.js
+++ users-service/src/server/routes.js
@@ -6,32 +6,6 @@ import { addHours } from 'date-fns'
 const USER_SESSION_EXPIRY_HOURS = 1
 
 const setupRoutes = app => {
-  app.get('/users', async (req, res, next) => {
-    const users = await User.findAll()
-    if (users.length > 0) {
-      return res.json(users)
-    }
-    return res.json({ message: 'Dont have any user!' })
-  })
-
-  app.post('/users', async (req, res, next) => {
-    if (!req.body.email || !req.body.password) {
-      return next(new Error('Invalid body!'))
-    }
-
-    try {
-      const newUser = await User.create({
-        id: generateUUID(),
-        email: req.body.email,
-        password: hashPassword(req.body.password),
-      })
-
-      return res.json(newUser)
-    } catch (e) {
-      return next(e)
-    }
-  })
-
   app.post('/sessions', async (req, res, next) => {
     if (!req.body.email || !req.body.password) {
       return next(new Error('Invalid body!'))
@@ -56,6 +30,56 @@ const setupRoutes = app => {
         expiresAt,
       })
       return res.json({ userSession, user })
+    } catch (e) {
+      return next(e)
+    }
+  })
+
+  app.get('/sessions/:sessionId', async (req, res, next) => {
+    try {
+      const userSession = await UserSession.findByPk(req.params.sessionId)
+
+      if (!userSession) return next(new Error('Invalid session ID'))
+
+      return res.json(userSession)
+    } catch (e) {
+      return next(e)
+    }
+  })
+
+  app.post('/users', async (req, res, next) => {
+    if (!req.body.email || !req.body.password) {
+      return next(new Error('Invalid body!'))
+    }
+
+    try {
+      const newUser = await User.create({
+        id: generateUUID(),
+        email: req.body.email,
+        password: hashPassword(req.body.password),
+      })
+
+      return res.json(newUser)
+    } catch (e) {
+      return next(e)
+    }
+  })
+
+  app.get('/users', async (req, res, next) => {
+    const users = await User.findAll()
+    if (users.length > 0) {
+      return res.json(users)
+    }
+    return res.json({ message: 'Dont have any user!' })
+  })
+
+  app.get('/users/:userId', async (req, res, next) => {
+    try {
+      const user = await User.findByPk(req.params.userId)
+
+      if (!user) return next(new Error('Invalid user ID'))
+
+      return res.json(user)
     } catch (e) {
       return next(e)
     }
