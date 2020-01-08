90df07a1dcf7b90b57d784fd5d08c9d9643cb59c
Demons Light <jetaimefrc@gmail.com>
2020-01-08 22:12:25 +0700

    build: basic clients

--- client-app/src/index.js
+++ client-app/src/index.js
@@ -2,10 +2,12 @@ import '@babel/polyfill'
 import React from 'react'
 import { ApolloProvider } from 'react-apollo'
 import { render } from 'react-dom'
+import { Provider } from 'react-redux'
 import { createGlobalStyle, ThemeProvider } from 'styled-components'
 
 import graphqlClient from '#root/api/graphqlClient'
 import Root from '#root/components/Root'
+import store from '#root/store'
 
 import * as theme from './theme'
 
@@ -23,11 +25,13 @@ const GlobalStyle = createGlobalStyle`
 `
 
 render(
-  <ApolloProvider client={graphqlClient}>
-    <ThemeProvider theme={theme}>
-      <GlobalStyle />
-      <Root />
-    </ThemeProvider>
-  </ApolloProvider>,
+  <Provider store={store}>
+    <ApolloProvider client={graphqlClient}>
+      <ThemeProvider theme={theme}>
+        <GlobalStyle />
+        <Root />
+      </ThemeProvider>
+    </ApolloProvider>
+  </Provider>,
   document.getElementById('app'),
 )
