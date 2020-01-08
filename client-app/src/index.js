abbd29b65ed7e8e20fbaba710f2939cc84da1b35
Demons Light <jetaimefrc@gmail.com>
2020-01-08 21:44:39 +0700

    chore: add constant colors for app

--- client-app/src/index.js
+++ client-app/src/index.js
@@ -1,5 +1,33 @@
+import '@babel/polyfill'
 import React from 'react'
+import { ApolloProvider } from 'react-apollo'
 import { render } from 'react-dom'
+import { createGlobalStyle, ThemeProvider } from 'styled-components'
+
+import graphqlClient from '#root/api/graphqlClient'
 import Root from '#root/components/Root'
 
-render(<Root />, document.getElementById('app'))
+import * as theme from './theme'
+
+const GlobalStyle = createGlobalStyle`
+  @import url('https://fonts.googleapis.com/css?family=Roboto:400,700&display=swap');
+  html, body, #app {
+    height: 100%;
+    margin: 0;
+    padding: 0;
+    width: 100%;
+  }
+  body {
+    font-family: Roboto, sans-serif;
+  }
+`
+
+render(
+  <ApolloProvider client={graphqlClient}>
+    <ThemeProvider theme={theme}>
+      <GlobalStyle />
+      <Root />
+    </ThemeProvider>
+  </ApolloProvider>,
+  document.getElementById('app'),
+)
---
+++ client-app/src/theme.js
@@ -0,0 +1,3 @@
+// Define constant colors to make the consistency in our app
+
+export const lightBlue300 = '#4FC3F7'
