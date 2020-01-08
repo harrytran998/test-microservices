073162b1ad0e8b72ce93ebe4d278062530b9ba9a
Demons Light <jetaimefrc@gmail.com>
2020-01-08 20:50:34 +0700

    chore: add custom resolver user to add in usersession

--- api-gateway/src/graphql/resolvers/index.js
+++ api-gateway/src/graphql/resolvers/index.js
@@ -1,6 +1,7 @@
 import * as Mutation from './Mutation'
 import * as Query from './Query'
+import * as UserSession from './UserSession'
 
-const resolvers = { Query, Mutation }
+const resolvers = { Query, Mutation, UserSession }
 
 export default resolvers
