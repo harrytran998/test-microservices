eefafa252abd65ba2abef4539aac60acff5a0068
Demons Light <jetaimefrc@gmail.com>
2020-01-14 20:56:43 +0700

    chore: add context user must loggin to add listing

--- api-gateway/src/graphql/resolvers/Mutation/createListings.js
+++ api-gateway/src/graphql/resolvers/Mutation/createListings.js
@@ -1,6 +1,7 @@
 import ListingsService from '#root/adapters/ListingsService'
 
-const createListingResolver = async (obj, { title, description }) => {
+const createListingResolver = async (obj, { title, description }, { res }) => {
+  if (!res.locals.userSession) throw new Error('Not logged in!')
   return await ListingsService.createListings({ title, description })
 }
 
