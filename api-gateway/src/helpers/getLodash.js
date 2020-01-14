32ee066f03e8663d418a00cd9009cabe8c79ef0e
Demons Light <jetaimefrc@gmail.com>
2020-01-14 21:58:39 +0700

    build: fix hardcode env

--- api-gateway/src/helpers/getLodash.js
+++
@@ -1,9 +0,0 @@
-const get = (obj, path, defaultValue) => {
-  const result = String.prototype.split
-    .call(path, /[,[\].]+?/)
-    .filter(Boolean)
-    .reduce((res, key) => (res !== null && res !== undefined ? res[key] : res), obj)
-  return result === undefined || result === obj ? defaultValue : result
-}
-
-export default get
